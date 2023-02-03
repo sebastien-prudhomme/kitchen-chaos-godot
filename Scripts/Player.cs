0373c55d9ec4bc3fa188d055073363da9725f737
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2023-02-03 22:41:23 +0100

    WIP

---
+++ Scripts/Player.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class Player : Node3D {
+	private float moveSpeed = 7f;
+	
+	public override void _Process(double delta) {
+		Vector2 inputVector = new Vector2(0f ,0f);
+
+        if (Input.IsKeyPressed(Key.Z)) {
+            inputVector.Y = 1f;
+        }
+
+        if (Input.IsKeyPressed(Key.S)) {
+            inputVector.Y = -1f;
+        }
+        
+        if (Input.IsKeyPressed(Key.Q)) {
+            inputVector.X = 1f;
+        }
+
+        if (Input.IsKeyPressed(Key.D)){
+            inputVector.X = -1f;
+        }        
+
+        if (inputVector.Length() > 0f) {
+            inputVector = inputVector.Normalized();
+
+            Vector3 moveDir = new Vector3(inputVector.X, 0f, inputVector.Y);
+            Position += moveDir * moveSpeed * (float)delta;
+
+            float rotateSpeed = 10f;
+            LookAt(Position - Basis.Z.Slerp(moveDir, (float)delta * rotateSpeed));
+        }
+	}
+}
