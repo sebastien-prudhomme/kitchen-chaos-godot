3a1bfc6529f3b3d4337584546d46a7f7d702cb60
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2023-02-04 16:21:35 +0100

    WIP

--- Scripts/PlayerAnimationTree.cs
+++ Scripts/PlayerAnimationTree.cs
@@ -3,5 +3,12 @@ using System;
 
 public partial class PlayerAnimationTree : AnimationTree {
 	[Export]
+	private Player player;
+
+	[Export]
     private bool isWalking;
+
+	public override void _Process(double delta) {
+		isWalking = player.IsWalking();
+	}
 }
