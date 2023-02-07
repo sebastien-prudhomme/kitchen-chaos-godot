70e847569a4621f2d1bc33964afdc992e432b7a9
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2023-02-07 22:59:26 +0100

    WIP

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,15 +4,28 @@ using System;
 public partial class Player : Node3D {
 	[Export] private float moveSpeed = 7f;
     [Export] private GameInput gameInput;
+    [Export] private ShapeCast3D shapeCast;
 
     private bool isWalking;
-	
-	public override void _Process(double delta) {
+
+	public override void _PhysicsProcess(double delta) {
 		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         if (inputVector.Length() > 0f) {
             Vector3 moveDir = new Vector3(inputVector.X, 0f, inputVector.Y);
-            Position += moveDir * moveSpeed * (float)delta;
+
+            float moveDistance = moveSpeed * (float)delta;
+
+            // Problème à faire fonctionner en tant qu'enfant
+            shapeCast.GlobalPosition = GlobalPosition + Vector3.Up;
+            shapeCast.TargetPosition = moveDir * moveDistance;
+            shapeCast.ForceShapecastUpdate();
+
+            bool canMove = !shapeCast.IsColliding();
+
+            if (canMove) {
+                Position += moveDir * moveDistance;
+            }
 
             float rotateSpeed = 10f;
             LookAt(Position - Basis.Z.Slerp(moveDir, (float)delta * rotateSpeed));
